4926c08dec5e73a40ee85e279afec4864307c15d
Muktadir (Arsalan) <128473682+MkdirRaiden@users.noreply.github.com>
2024-08-22 17:23:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,7 @@ import Features from "./components/Features";
 import HowItWorks from "./components/HowItWorks";
 import Footer from "./components/Footer";
 const App = () => {
-  if (isLoading) {
-    return (
-      <>
-        <div className="flex max-w-full-screen-2xl h-screen justify-center items-center">
-          <span className="loading loading-spinner loading-lg"></span>
-        </div>
-      </>
-    );
-  }
+
   return (
     <>
       <main className="bg-black">
