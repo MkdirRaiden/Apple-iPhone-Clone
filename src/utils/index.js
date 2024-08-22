aded27ad973b6db5f1e9408041269248c74173bc
Muktadir (Arsalan) <128473682+MkdirRaiden@users.noreply.github.com>
2024-08-22 17:29:45 +0530

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -56,4 +56,4 @@ export const explore1Img = explore1;
 export const explore2Img = explore2;
 export const chipImg = chip;
 export const frameImg = frame;
-export const postureImg = posture;
+export const posterImg = posture;
